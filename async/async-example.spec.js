bf08dcac7ca82ae39fcc4e831faa49b8e5f04bd8
theopmw <theowrightdev@gmail.com>
2023-03-23 17:23:37 +0000

    Fix generateToken() test

--- async/async-example.spec.js
+++ async/async-example.spec.js
@@ -1,12 +1,17 @@
 import { describe, expect, it } from 'vitest';
 import { generateToken } from './async-example';
 
-describe('generateToken()', () => {
+describe('generateToken()', (done) => {
   it('should generate a token value', () => {
     const testUserEmail = 'test@test.com';
 
     generateToken(testUserEmail, (err, token) => {
-      expect(token).toBeDefined();
+      try {
+        expect(token).toBeDefined();
+        done();
+      } catch (err) {
+        done(err);
+      }
     });
   });
 });
